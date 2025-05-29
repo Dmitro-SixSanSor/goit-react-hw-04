f30f4998df0f06d3dcf6884e240f23425800e159
Dmitro-SixSanSor <dmytro.ostaphiychuk@gmail.com>
2025-05-29 17:09:37 +0300

    Delete src/components/errormessage directory

--- src/components/errormessage/ErrorMesage.jsx
+++
@@ -1,9 +0,0 @@
-import css from './ErrorMessage.module.css';
-
-export function ErrorMessage({ message }) {
-  return (
-    <div className={css.wrapper}>
-      <p className={css.text}>{message}</p>
-    </div>
-  );
-}
